3d0a0ab1e2d948b74ac057adce3c7897905609a3
jnicholson-wcgs <54621945+jnicholson-wcgs@users.noreply.github.com>
2020-05-11 18:33:13 +0100

    Updates to dummy print()

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 
 def mainmenu() :
 
+  print ("Have you inserted your Python Cricket 2 code above?\nIf so remove this line!\n")
   print ("Please select from the following options.....")
   #
   # COMPLETE THE REST OF THE MAIN MENU CODE HERE
