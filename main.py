09005b05eacf912ee611214317c996e539e16dc8
jnicholson-wcgs <54621945+jnicholson-wcgs@users.noreply.github.com>
2020-05-11 18:25:28 +0100

    Initial version

---
+++ main.py
@@ -0,0 +1,16 @@
+#
+#
+# INSERT YOUR PYTHON CRICKET 2 CODE HERE
+#
+#
+
+
+def mainmenu() :
+
+  print ("Please select from the following options.....")
+  #
+  # COMPLETE THE REST OF THE MAIN MENU CODE HERE
+  #
+  
+
+mainmenu()
